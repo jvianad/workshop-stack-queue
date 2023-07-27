0b63bd27ceef057dfd18fbdf9d4fa4696ec1421d
jvianad <jeanvianademares@gmail.com>
2023-07-26 21:09:43 -0500

    Validador de html

---
+++ src/HtmlValidator.java
@@ -0,0 +1,25 @@
+import java.util.Queue;
+import java.util.Stack;
+
+public class HtmlValidator {
+    //creamos un validados de tipo stack que reciba como paramatro una cola(queue)
+    public static Stack<HtmlTag> isValidHtml(Queue<HtmlTag> tags) {
+        Stack<HtmlTag> stack = new Stack<>();
+
+        while (!tags.isEmpty()) {
+            HtmlTag tag = tags.remove();
+            if (tag.isOpenTag()) {
+                stack.push(tag);
+            } else {
+                if (stack.isEmpty() || !tag.matches(stack.peek())) {
+                    // aqui checkeamos que la pila está vacía o la etiqueta de cierre no coincide con la última etiqueta de apertura
+                    return stack; // La pila contiene etiquetas sin cerrar, lo que significa que el HTML no es válido
+                }
+                stack.pop(); // La etiqueta de cierre coincide con la última etiqueta de apertura, así que se saca
+            }
+        }
+
+        return stack; // aqui me devuelve el stack vacio si esta well formated o bien formateado
+    }
+}
+
---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import java.io.IOException;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String filePath = "C:\\Users\\Jean Viaña\\Desktop\\validator\\src\\example.html";
+
+        // Leemos el archivo HTML y obtenga las etiquetas como una cola
+        Queue<HtmlTag> tags = HtmlReader.getTagsFromHtmlFile(filePath);
+
+        // validamos las etiquetas html
+        Stack<HtmlTag> result = HtmlValidator.isValidHtml(tags);
+
+        if (result.isEmpty()) {
+            System.out.println("The HTML file is well-formatted.");
+        } else {
+            System.out.println("The HTML file is invalid. Unclosed tags:");
+            while (!result.isEmpty()) {
+                HtmlTag unclosedTag = result.pop();
+                System.out.println(unclosedTag.getElement());
+            }
+        }
+
+        System.out.println();
+
+        String filePath2 = "C:\\Users\\Jean Viaña\\Desktop\\validator\\src\\example2.html";
+
+        // Leemos el archivo HTML y obtenga las etiquetas como una cola
+        Queue<HtmlTag> tags2 = HtmlReader.getTagsFromHtmlFile(filePath2);
+
+        // validamos las etiquetas html
+        Stack<HtmlTag> result2 = HtmlValidator.isValidHtml(tags2);
+
+        if (result2.isEmpty()) {
+            System.out.println("The HTML file is well-formatted.");
+        } else {
+            System.out.println("The HTML file is invalid. Unclosed tags:");
+            while (!result2.isEmpty()) {
+                HtmlTag unclosedTag = result2.pop();
+                System.out.println(unclosedTag.getElement());
+            }
+        }
+
+    }
+}
